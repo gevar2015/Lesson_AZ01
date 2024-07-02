d068db63bc2ddc351d2d5820f760b5d0de15f247
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-07-02 21:50:32 +0300

    Update salary.py
    Группировка, расчет средней зарплаты, вывод на печать

--- salary.py
+++ salary.py
@@ -1,5 +1,14 @@
 import pandas as pd
+
+# Загрузка данных
 data = pd.read_csv("dz.csv")
 
-print(data.describe())
-print(data.head())
+# Печать информации о данных
+print(data.info())
+
+# Группировка данных по столбцу 'City' и вычисление среднего значения для столбца 'Salary'
+group = data.groupby('City')['Salary'].mean()
+
+# Печать результата
+print(group)
+
